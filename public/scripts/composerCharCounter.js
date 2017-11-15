9734d20bc60d75e7718c2a0381c4aa2fefdb78be
Kang Wang <kwang.inbox@gmail.com>
2017-11-14 16:45:10 -0800

    Implemented the counter property, correctly.

--- public/scripts/composerCharCounter.js
+++ public/scripts/composerCharCounter.js
@@ -1,4 +1,13 @@
-$(document).ready(function() {
-  alert("Composer character counter JS file loaded!");
-  alert("Document is ready!");
+$(document).ready(function(){
+  var maxLength = 140;
+
+  $('.new-tweet textarea').keyup(function() {
+  var length = $(this).val().length;
+  var length = maxLength-length;
+  $('.counter').text(length);
+
+  if(length < 0){
+    $('.counter').css('color', 'red')
+    }
+  });
 });
