cc47b441cbbd7cf48e8f0c62aa3389b4cc190b13
Kang Wang <kwang.inbox@gmail.com>
2017-11-16 11:04:05 -0800

    Implemented 'this' into composerCharCounter().

--- public/scripts/composerCharCounter.js
+++ public/scripts/composerCharCounter.js
@@ -1,17 +1,15 @@
 $(document).ready(function(){
-  console.log("The document is ready!");
   var maxLength = 140;
+  $('.new-tweet textarea').keyup(function() {
+  var length = $(this).val().length;
+  var length = maxLength-length;
+  $('.counter').text(length);
+  if (length < 0){
+    $('.counter').css("color", "red");
+  }
+  else {
+     $('.counter').css("color", "black");
+  }
 
-    $(".new-tweet textarea").keyup(function() {
-    var length = $(this).val().length;
-    var length = maxLength-length;
-    // $(".counter").text(length);
-    $(this).siblings(".counter").text(length);
-
-    if(length < 0){
-      $(this).siblings(".counter").css("color", "red");
-    } else {
-      $(this).siblings(".counter").css("color", "black");
-    }
-  });
 });
+})
