188f3aa9811a58604b830319eef414e752a87876
Kang <kwang.inbox@gmail.com>
2017-11-16 21:29:46 +0000

    Revised app JS.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,53 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $( function () {
-  // var tweetData = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-  //         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-  //         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-  //       },
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-  //         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-  //         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-  //       },
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Johann von Goethe",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-  //         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-  //         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-  //       },
-  //       "handle": "@johann49"
-  //     },
-  //     "content": {
-  //       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-  //     },
-  //     "created_at": 1461113796368
-  //   }
-  // ];
-
   function escape(str) {
     var div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
@@ -59,24 +12,26 @@ $( function () {
 
   function createTweetElement(tweet) {
     // ...
-    var tweetArticle = `<article class = "tweetPlz">
+    var tweetArticle = `<article class="tweet">
                           <header class="headerbox">
-                            <img src = ${escape(tweet.user.avatars.large)} height="42" width="42">
-                              <h1>${escape(tweet.user.name)}</h1>
-                              <h2>${escape(tweet.user.handle)}</h2>
+                            <div class="userInfo">
+                              <img class="avatar" src=${escape(tweet.user.avatars.large)} alt="User Avatar">
+                              <h1 class="username">${escape(tweet.user.name)}</h1>
+                            </div>
+                            <h2 class="userhandle">${escape(tweet.user.handle)}</h2>
                           </header>
-                            <p class="text">
-                              ${escape(tweet.content.text)}
-                            </p>
-                            <footer>
-                              <p>${escape(tweet.created_at)}</p>
-                              <div class="icons">
-                                <i class="fa fa-flag" aria-hidden="true"></i>
-                                <i class="fa fa-retweet" aria-hidden="true"></i>
-                                <i class="fa fa-heart" aria-hidden="true"></i>
-                              </div>
-                            </footer>
-                      </article>`
+                          <div class="text">
+                            ${escape(tweet.content.text)}
+                          </div>
+                          <footer class="footerbox">
+                            <div>${escape(tweet.created_at)}</div>
+                            <div class="icons">
+                              <i class="fa fa-flag" aria-hidden="true"></i>
+                              <i class="fa fa-retweet" aria-hidden="true"></i>
+                              <i class="fa fa-heart" aria-hidden="true"></i>
+                            </div>
+                          </footer>
+                        </article>`
     return tweetArticle;
   }
 
@@ -134,7 +89,7 @@ $( function () {
 
   $(".new-tweet").hide();
     $("#compose-button").click(function(event) {
-      $(".new-tweet").toggle();
+      $(".new-tweet").slideToggle();
       $("textarea").focus();
     })
 })
