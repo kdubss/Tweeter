5ffb57fa175eb0ad392fdb8fc01169c8eff121dd
Kang <kwang.inbox@gmail.com>
2017-11-17 01:19:45 +0000

    Refactored getTweet and saveTweet functions to get/save into mongodb tweet database.

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,17 +9,26 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      // db.tweets.push(newTweet);
+      db.collection("tweets").insertOne(newTweet);
+      callback(null, true);
     },
 
     // Get all tweets in `db`, sorted by newest first
+    // getTweets: function(callback) {
+      // simulateDelay(() => {
+      //   const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      //   callback(null, db.tweets.sort(sortNewestFirst));
+      // });
+    // }
+    // Get all tweets from Mongo DB:
     getTweets: function(callback) {
-      simulateDelay(() => {
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err);
+        }
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+        callback(null, tweets.sort(sortNewestFirst));
       });
     }
 
