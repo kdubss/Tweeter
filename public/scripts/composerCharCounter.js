9ebcba60186bea284f743730c91606cb4cdaad66
Kang Wang <kwang.inbox@gmail.com>
2017-11-19 15:12:41 -0800

    Removed blank lines.

--- public/scripts/composerCharCounter.js
+++ public/scripts/composerCharCounter.js
@@ -1,15 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function() {
   var maxLength = 140;
   $('.new-tweet textarea').keyup(function() {
   var length = $(this).val().length;
   var length = maxLength-length;
   $('.counter').text(length);
-  if (length < 0){
+  if (length < 0) {
     $('.counter').css("color", "red");
-  }
-  else {
+  } else {
      $('.counter').css("color", "black");
   }
-
-});
+  });
 })
